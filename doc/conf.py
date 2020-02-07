ef66ee3c2a9735e38562f45e438310978d003e4c
Guilherme Leobas <gleobas@cyral.com>
2020-02-07 00:06:05 -0300

    Update conf.py
    Fixes the api page on readthedocs being empty

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 import docutils
 
 extensions = [
@@ -25,7 +28,7 @@ project = 'rbc'
 copyright = '2019, Xnd-Project'
 version = 'v0.1.0'
 release = 'v0.1.0'
-exclude_patterns = ['doc', 'build']
+exclude_patterns = ['doc', '_build']
 pygments_style = 'sphinx'
 
 html_static_path = ['_static']
