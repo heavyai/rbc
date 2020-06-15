1cc8271e2b3dfc2c310aff014afa400b48f1afae
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-06-15 13:50:34 -0300

    Update setup.py
    llvmlite=0.33.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def setup_package():
         setup_requires = []
         tests_require = []
     else:
-        install_requires = ["numba", "llvmlite>=0.29", "tblib", "thriftpy2",
+        install_requires = ["numba", "llvmlite>=0.33.0", "tblib", "thriftpy2",
                             "six", "netifaces"]
         setup_requires = ['pytest-runner']
         tests_require = ['pytest']
