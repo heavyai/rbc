2604c2eb730addc98707994800b2e5db3a9a832d
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-06-25 20:45:00 -0500

    implement unary ufuncs

--- rbc/omnisci_backend/numpy_math.py
+++ rbc/omnisci_backend/numpy_math.py
@@ -1,15 +1,10 @@
 import numpy as np
-from rbc import typesystem
-from rbc.irtools import printf
 from .omnisci_array import Array, ArrayPointer
 from rbc.utils import get_version
 if get_version('numba') >= (0, 49):
-    from numba.core import extending, types, \
-        errors
-    from numba.np import numpy_support
+    from numba.core import extending, types
 else:
-    from numba import extending, types, \
-        errors, numpy_support
+    from numba import extending, types
 
 
 def overload_elementwise_binary_ufunc(ufunc, name=None, dtype=None):
@@ -27,6 +22,7 @@ def overload_elementwise_binary_ufunc(ufunc, name=None, dtype=None):
                 nb_dtype = a.eltype
             else:
                 nb_dtype = dtype
+
             def impl(a, b):
                 sz = len(a)
                 x = Array(sz, nb_dtype)
@@ -34,7 +30,7 @@ def overload_elementwise_binary_ufunc(ufunc, name=None, dtype=None):
                     x[i] = nb_dtype(ufunc(a[i], b[i]))
                 return x
             return impl
-    
+
     decorate = extending.overload(ufunc)
 
     def wrapper(overload_func):
@@ -81,29 +77,108 @@ def dummy_binary_ufunc(a, b):
     pass
 
 
-def overload_unary_ufunc(ufunc, name=None):
+def overload_elementwise_unary_ufunc(ufunc, name=None, dtype=None):
     """
-    Helper for unary ufuncs that returns an array
+    Wrapper for unary ufuncs that returns an array
     """
     if name is None:
         name = ufunc.__name__
     globals()[name] = ufunc
 
-    def binary_ufunc_impl(a, b):
-        # XXX: raise error if len(a) != len(b)
-        if isinstance(a, ArrayPointer) and isinstance(b, ArrayPointer):
-            nb_dtype = a.eltype
-            def impl(a, b):
+    def unary_elementwise_ufunc_impl(a):
+        if isinstance(a, ArrayPointer):
+            if dtype is None:
+                nb_dtype = a.eltype
+            else:
+                nb_dtype = dtype
+
+            def impl(a):
                 sz = len(a)
                 x = Array(sz, nb_dtype)
                 for i in range(sz):
-                    x[i] = nb_dtype(ufunc(a[i], b[i]))
+                    x[i] = nb_dtype(ufunc(a[i]))
                 return x
             return impl
-    
+
     decorate = extending.overload(ufunc)
 
     def wrapper(overload_func):
-        return decorate(binary_ufunc_impl)
+        return decorate(unary_elementwise_ufunc_impl)
 
     return wrapper
+
+
+# Math operations
+@overload_elementwise_unary_ufunc(np.negative)
+@overload_elementwise_unary_ufunc(np.positive)
+@overload_elementwise_unary_ufunc(np.absolute)
+@overload_elementwise_unary_ufunc(np.fabs)
+@overload_elementwise_unary_ufunc(np.rint)
+# @overload_elementwise_unary_ufunc(np.absolute) # test?
+# @overload_elementwise_unary_ufunc(np.conj) # test?
+# @overload_elementwise_unary_ufunc(np.conjugate) # test?
+@overload_elementwise_unary_ufunc(np.exp)
+@overload_elementwise_unary_ufunc(np.exp2)
+@overload_elementwise_unary_ufunc(np.log)
+@overload_elementwise_unary_ufunc(np.log2)
+@overload_elementwise_unary_ufunc(np.log10)
+@overload_elementwise_unary_ufunc(np.expm1)
+@overload_elementwise_unary_ufunc(np.log1p)
+@overload_elementwise_unary_ufunc(np.sqrt)
+@overload_elementwise_unary_ufunc(np.square)
+# @overload_elementwise_unary_ufunc(np.cbrt) # not supported
+@overload_elementwise_unary_ufunc(np.reciprocal)
+# trigonometric functions
+@overload_elementwise_unary_ufunc(np.sin)
+@overload_elementwise_unary_ufunc(np.cos)
+@overload_elementwise_unary_ufunc(np.tan)
+@overload_elementwise_unary_ufunc(np.arcsin)
+@overload_elementwise_unary_ufunc(np.arccos)
+@overload_elementwise_unary_ufunc(np.arctan)
+@overload_elementwise_unary_ufunc(np.sinh)
+@overload_elementwise_unary_ufunc(np.cosh)
+@overload_elementwise_unary_ufunc(np.tanh)
+@overload_elementwise_unary_ufunc(np.arcsinh)
+@overload_elementwise_unary_ufunc(np.arccosh)
+@overload_elementwise_unary_ufunc(np.arctanh)
+@overload_elementwise_unary_ufunc(np.deg2rad)
+@overload_elementwise_unary_ufunc(np.rad2deg)
+# Comparison functions
+@overload_elementwise_unary_ufunc(np.logical_not, dtype=types.int8)
+# Floating functions
+@overload_elementwise_unary_ufunc(np.isfinite, dtype=types.int8)
+@overload_elementwise_unary_ufunc(np.isinf, dtype=types.int8)
+@overload_elementwise_unary_ufunc(np.isnan, dtype=types.int8)
+@overload_elementwise_unary_ufunc(np.fabs, dtype=types.double)
+@overload_elementwise_unary_ufunc(np.floor, dtype=types.double)
+@overload_elementwise_unary_ufunc(np.ceil, dtype=types.double)
+@overload_elementwise_unary_ufunc(np.trunc, dtype=types.double)
+# not supported?
+# @overload_elementwise_unary_ufunc(np.isnat, dtype=types.int8)
+# @overload_elementwise_unary_ufunc(np.signbit, dtype=types.int8)
+# @overload_elementwise_unary_ufunc(np.spacing, dtype=types.double)
+def dummy_unary_ufunc(a):
+    pass
+
+
+def heaviside(x1, x2):
+    pass
+
+
+@extending.overload(heaviside)
+def impl_np_heaviside(x1, x2):
+    if isinstance(x1, ArrayPointer):
+        nb_dtype = types.double
+
+        def impl(x1, x2):
+            sz = len(x1)
+            r = Array(sz, nb_dtype)
+            for i in range(sz):
+                if x1[i] < 0:
+                    r[i] = nb_dtype(0)
+                elif x1[i] == 0:
+                    r[i] = nb_dtype(x2)
+                else:
+                    r[i] = nb_dtype(1)
+            return r
+        return impl
--- rbc/tests/test_omnisci_array_math.py
+++ rbc/tests/test_omnisci_array_math.py
@@ -1,11 +1,6 @@
 import pytest
 import numpy as np
-import rbc.omnisci_backend as omni
-from rbc.utils import get_version
-if get_version('numba') >= (0, 49):
-    from numba.core import types
-else:
-    from numba import types
+import rbc.omnisci_backend as omni  # noqa: F401
 
 
 rbc_omnisci = pytest.importorskip('rbc.omniscidb')
@@ -57,6 +52,7 @@ def omnisci():
     except Exception as msg:
         print('%s in deardown' % (type(msg)))
 
+
 binary_fns = [
     ('add', 'int32[](int32[], int32[])', 'i4'),
     ('subtract', 'double[](double[], double[])', 'f8'),
@@ -97,17 +93,111 @@ def test_omnisci_array_binary_math_fns(omnisci, method, signature, column):
     s = f'def np_{method}(a, b): return omni.{method}(a, b)'
     exec(s, globals())
 
-    fn = omnisci(signature)(eval('np_{}'.format(method)))
+    omnisci(signature)(eval('np_{}'.format(method)))
 
     query = f'select {column}, ' + \
             f'np_{method}({column}, {column})' + \
             f' from {omnisci.table_name};'
 
     _, result = omnisci.sql_execute(query)
-    
+
     row, out = list(result)[0]
     expected = getattr(np, method)(row, row)
     if method == 'power':
-        assert np.isclose(expected, out, equal_nan=True).all(), 'omni_' + method
+        assert np.isclose(expected, out, equal_nan=True).all(), 'omni_' + method  # noqa: E501
     else:
         assert np.array_equal(expected, out), 'omni_' + method
+
+
+unary_fns = [
+    # math operations
+    ('negative', 'int64[](int64[])', 'i8'),
+    # ('positive', 'int64[](int64[])', 'i8'),
+    # ('absolute', 'int64[](double[])', 'i8'),
+    ('absolute', 'double[](double[])', 'f8'),
+    ('fabs', 'double[](double[])', 'f8'),
+    ('rint', 'double[](double[])', 'f8'),
+    # ('absolute'), 'double[](double[])', 'f8'),
+    # ('conj'), 'double[](double[])', 'f8'),
+    # ('conjugate'), 'double[](double[])', 'f8'),
+    ('exp', 'double[](double[])', 'f8'),
+    ('exp2', 'double[](double[])', 'f8'),
+    ('log', 'double[](double[])', 'f8'),
+    ('log2', 'double[](double[])', 'f8'),
+    ('log10', 'double[](double[])', 'f8'),
+    ('expm1', 'double[](double[])', 'f8'),
+    ('log1p', 'double[](double[])', 'f8'),
+    ('sqrt', 'double[](double[])', 'f8'),
+    ('square', 'double[](double[])', 'f8'),
+    # ('cbrt', 'double[](double[])', 'f8'),
+    ('reciprocal', 'double[](double[])', 'f8'),
+    # trigonometric functions
+    ('sin', 'double[](double[])', 'f8'),
+    ('cos', 'double[](double[])', 'f8'),
+    ('tan', 'double[](double[])', 'f8'),
+    ('arcsin', 'double[](double[])', 'f8'),
+    ('arccos', 'double[](double[])', 'f8'),
+    ('arctan', 'double[](double[])', 'f8'),
+    ('sinh', 'double[](double[])', 'f8'),
+    ('cosh', 'double[](double[])', 'f8'),
+    ('tanh', 'double[](double[])', 'f8'),
+    ('arcsinh', 'double[](double[])', 'f8'),
+    ('arccosh', 'double[](double[])', 'f8'),
+    ('arctanh', 'double[](double[])', 'f8'),
+    ('deg2rad', 'double[](double[])', 'f8'),
+    ('rad2deg', 'double[](double[])', 'f8'),
+    # comparison functions
+    ('logical_not', 'int8[](int64[])', 'i8'),
+    # floating functions
+    ('isfinite', 'int8[](int64[])', 'i8'),
+    ('isinf', 'int8[](int64[])', 'i8'),
+    ('isnan', 'int8[](int64[])', 'i8'),
+    # ('isnat', 'int8[](int64[])', 'i8'), # doesn't work
+    ('fabs', 'double[](double[])', 'f8'),
+    # ('signbit', 'int8[](int64[])', 'i8'), # not supported
+    # ('spacing', 'double[](double[])', 'f8'), # not supported
+    ('floor', 'double[](double[])', 'f8'),
+    ('ceil', 'double[](double[])', 'f8'),
+    ('trunc', 'double[](double[])', 'f8'),
+]
+
+
+@pytest.mark.parametrize("method, signature, column", unary_fns,
+                         ids=[item[0] for item in unary_fns])
+def test_omnisci_array_unary_math_fns(omnisci, method, signature, column):
+    omnisci.reset()
+
+    s = f'def np_{method}(a): return omni.{method}(a)'
+    exec(s, globals())
+
+    omnisci(signature)(eval('np_{}'.format(method)))
+
+    query = f'select {column}, ' + \
+            f'np_{method}({column})' + \
+            f' from {omnisci.table_name};'
+
+    _, result = omnisci.sql_execute(query)
+
+    row, out = list(result)[0]
+    expected = getattr(np, method)(row)
+    _isclose = ['log', 'log2', 'log10', 'log1p', 'sqrt',
+                'arcsin', 'arccos', 'arccosh', 'arctanh']
+    if method in _isclose:
+        assert np.isclose(expected, out, equal_nan=True).all(), 'omni_' + method  # noqa: E501
+    else:
+        assert np.array_equal(expected, out), 'omni_' + method
+
+
+def test_heaviside(omnisci):
+
+    @omnisci('double[](int64[], int64)')
+    def heaviside(x1, x2):
+        return omni.heaviside(x1, x2)
+
+    query = f'select i8, heaviside(i8, 1) from {omnisci.table_name}'
+    _, result = omnisci.sql_execute(query)
+    result = list(result)
+
+    for inp, out in result:
+        expected = np.heaviside(inp, 1)
+        np.array_equal(expected, out)
