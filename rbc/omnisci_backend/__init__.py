a54025f0f56fc9c41c0fdfa9f5dee3abe733a140
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-07-20 20:29:48 -0500

    rename file

--- rbc/omnisci_backend/__init__.py
+++ rbc/omnisci_backend/__init__.py
@@ -1,5 +1,5 @@
 from .omnisci_array import *  # noqa: F401, F403
 from .python_operators import *  # noqa: F401, F403
 from .numpy_funcs import *  # noqa: F401, F403
-from .numpy_ufunc import *  # noqa: F401, F403
+from .numpy_ufuncs import *  # noqa: F401, F403
 from .omnisci_pipeline import *  # noqa: F401, F403
