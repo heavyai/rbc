f80c8f1193f504c63c940ac6448b827fd0ccefea
Guilherme Leobas <guilhermeleobas@gmail.com>
2020-07-21 14:40:24 -0300

    disable tests for omniscidb 5.3.1

--- rbc/tests/test_omnisci_array_operators.py
+++ rbc/tests/test_omnisci_array_operators.py
@@ -492,6 +492,17 @@ def test_array_operators(omnisci, suffix, signature, args, expected):
         pytest.skip(f'operator_{suffix}: crashes CUDA enabled omniscidb server'
                     ' [rbc issue 107]')
 
+    if (available_version[:3] == (5, 3, 1)
+        and suffix in ['abs', 'add', 'and_bw', 'eq', 'floordiv', 'floordiv2',
+                       'ge', 'gt', 'iadd', 'iand', 'ifloordiv', 'ifloordiv2',
+                       'ilshift', 'imul', 'ior', 'isub', 'ipow', 'irshift',
+                       'itruediv', 'itruediv2', 'imod', 'ixor', 'le', 'lshift',
+                       'lt', 'mul', 'mod', 'ne', 'neg', 'or_bw', 'pos', 'pow',
+                       'rshift', 'sub', 'truediv', 'truediv2', 'xor']):
+        pytest.skip(
+            f'operator_{suffix}: crashes CPU-only omniscidb server v 5.3'
+            ' [issue 115]')
+
     omnisci.reset()
 
     omnisci(signature)(eval('operator_{}'.format(suffix)))
