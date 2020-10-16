ed21ac59d36fe41967dc03b132a33460b0b7baa6
Pearu Peterson <pearu.peterson@gmail.com>
2020-10-16 08:17:30 +0300

    Debug omnisci-ci env

--- start_omniscidb_bg.sh
+++ start_omniscidb_bg.sh
@@ -1,4 +1,5 @@
 conda activate omnisci-ci
+conda list
 mkdir data && omnisci_initdb data -f
 omnisci_server --version
 omnisci_server --enable-runtime-udf --enable-table-functions 2>&1 > omniscidb-output.txt &
