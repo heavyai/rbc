ddbcc5760c8a21e2b06de57d8a416d88e56a0003
Pearu Peterson <pearu.peterson@gmail.com>
2020-10-16 08:45:22 +0300

    Tmp fix to travis CI omnisci-ci env.

--- start_omniscidb_bg.sh
+++ start_omniscidb_bg.sh
@@ -1,5 +1,4 @@
 conda activate omnisci-ci
-conda list
 mkdir data && omnisci_initdb data -f
 omnisci_server --version
 omnisci_server --enable-runtime-udf --enable-table-functions 2>&1 > omniscidb-output.txt &
