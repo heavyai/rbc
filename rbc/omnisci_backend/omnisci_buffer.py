fc56148e5a73f0772d17e4912aabb9226c0e5994
Guilherme Leobas <gleobas@quansight.com>
2020-11-14 19:35:53 +0000

    add column test

--- rbc/omnisci_backend/omnisci_buffer.py
+++ rbc/omnisci_backend/omnisci_buffer.py
@@ -71,7 +71,7 @@ class BufferPointer(types.Type):
         self.dtype = dtype    # struct dtype
         self.eltype = eltype  # buffer element dtype
         name = "(%s)*" % dtype
-        types.Type.__init__(self, name)
+        super().__init__(name)
 
     @property
     def key(self):
@@ -173,37 +173,38 @@ def omnisci_buffer_getitem(x, i):
         return lambda x, i: omnisci_buffer_getitem_(x, i)
 
 
+# [rbc issue-197] Numba promotes operations like
+# int32(a) + int32(b) to int64
+def truncate_or_extend(builder, nb_value, eltype, value, buf_typ):
+    # buf[pos] = val
+
+    if isinstance(nb_value, types.Integer):  # Integer
+        if eltype.bitwidth < nb_value.bitwidth:
+            return builder.trunc(value, buf_typ)  # truncate
+        elif eltype.bitwidth > nb_value.bitwidth:
+            is_signed = nb_value.signed
+            return builder.sext(value, buf_typ) if is_signed else \
+                builder.zext(value, buf_typ)  # extend
+    elif isinstance(nb_value, types.Float):  # Floating-point
+        if eltype.bitwidth < nb_value.bitwidth:
+            return builder.fptrunc(value, buf_typ)  # truncate
+        elif eltype.bitwidth > nb_value.bitwidth:
+            return builder.fpext(value, buf_typ)  # extend
+    elif isinstance(nb_value, types.Boolean):
+        if buf_typ.width < value.type.width:
+            return builder.trunc(value, buf_typ)
+        elif buf_typ.width > value.type.width:
+            return builder.sext(value, buf_typ)
+
+    return value
+
+
 @extending.intrinsic
 def omnisci_buffer_ptr_setitem_(typingctx, data, index, value):
     sig = types.none(data, index, value)
 
     eltype = data.eltype
     nb_value = value
-
-    # [rbc issue-197] Numba promotes operations like
-    # int32(a) + int32(b) to int64
-    def truncate_or_extend(builder, value, buf_typ):
-        # buf[pos] = val
-
-        if isinstance(nb_value, types.Integer):  # Integer
-            if eltype.bitwidth < nb_value.bitwidth:
-                return builder.trunc(value, buf_typ)  # truncate
-            elif eltype.bitwidth > nb_value.bitwidth:
-                is_signed = nb_value.signed
-                return builder.sext(value, buf_typ) if is_signed else \
-                    builder.zext(value, buf_typ)  # extend
-        elif isinstance(nb_value, types.Float):  # Floating-point
-            if eltype.bitwidth < nb_value.bitwidth:
-                return builder.fptrunc(value, buf_typ)  # truncate
-            elif eltype.bitwidth > nb_value.bitwidth:
-                return builder.fpext(value, buf_typ)  # extend
-        elif isinstance(nb_value, types.Boolean):
-            if buf_typ.width < value.type.width:
-                return builder.trunc(value, buf_typ)
-            elif buf_typ.width > value.type.width:
-                return builder.sext(value, buf_typ)
-
-        return value
 
     def codegen(context, builder, signature, args):
         zero = int32_t(0)
@@ -214,7 +215,7 @@ def omnisci_buffer_ptr_setitem_(typingctx, data, index, value):
         ptr = builder.load(rawptr)
 
         buf = builder.load(builder.gep(ptr, [zero, zero]))
-        value = truncate_or_extend(builder, value, buf.type.pointee)
+        value = truncate_or_extend(builder, nb_value, eltype, value, buf.type.pointee)
         builder.store(value, builder.gep(buf, [index]))
 
     return sig, codegen
@@ -223,6 +224,9 @@ def omnisci_buffer_ptr_setitem_(typingctx, data, index, value):
 @extending.intrinsic
 def omnisci_buffer_setitem_(typingctx, data, index, value):
     sig = types.none(data, index, value)
+
+    eltype = data.members[0].dtype
+    nb_value = value
 
     def codegen(context, builder, signature, args):
         zero = int32_t(0)
@@ -235,6 +239,7 @@ def omnisci_buffer_setitem_(typingctx, data, index, value):
         ptr = builder.load(builder.gep(
             buf, [zero, zero]))
 
+        value = truncate_or_extend(builder, nb_value, eltype, value, ptr.type.pointee)
         builder.store(value, builder.gep(ptr, [index]))
 
     return sig, codegen
