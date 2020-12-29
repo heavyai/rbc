7502e1d3e3b50ff329df18aeb20aebd71f856cbe
Guilherme Leobas <gleobas@quansight.com>
2020-12-29 00:41:06 +0000

    flake8

--- rbc/irtools.py
+++ rbc/irtools.py
@@ -420,9 +420,9 @@ def compile_to_LLVM(functions_and_signatures,
         for func, signatures in functions_and_signatures:
             for fid, sig in signatures.items():
                 fname = compile_instance(func, sig, target_info, typing_context,
-                                        target_context, pipeline_class,
-                                        main_library,
-                                        debug=debug)
+                                         target_context, pipeline_class,
+                                         main_library,
+                                         debug=debug)
                 if fname is not None:
                     succesful_fids.append(fid)
                     function_names.append(fname)
