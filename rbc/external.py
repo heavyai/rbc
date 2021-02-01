c62a25be0be1d77383a560c5e237898ad07a8118
Guilherme Leobas <gleobas@quansight.com>
2021-02-01 17:55:20 +0000

    address comments

--- rbc/external.py
+++ rbc/external.py
@@ -4,10 +4,35 @@ from numba.core import extending, funcdesc, types
 from rbc.typesystem import Type
 
 
-class _External:
-    def __init__(self, symbol, signature):
-        self.symbol = symbol
+class External:
+    @classmethod
+    def fromobject(cls, signature, name=None):
+        # Make inner function for the actual work
+        t = Type.fromobject(signature)
+        if not t.is_function:
+            raise ValueError("signature must represent a function type")
+
+        if name is None:
+            name = t.name
+        if not name:
+            raise ValueError(
+                f"external function name not specified for signature {signature}"
+            )
+
+        return cls(name, t.tonumba())
+
+    def __init__(self, name: str, signature: types.FunctionType):
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the external function
+        signature : Numba function signature
+            A numba function type signature. i.e. (float64, float64) -> int64
+        """
         self._signature = signature
+        self.name = name
+        self.register()
 
     def register(self):
         # typing
@@ -18,7 +43,7 @@ class _External:
         )
 
         template = make_concrete_template(
-            self.symbol, key=self.symbol, signatures=[self.signature]
+            self.name, key=self.name, signatures=[self.signature]
         )
         infer(template)
         infer_global(self, types.Function(template))
@@ -26,18 +51,18 @@ class _External:
         # lowering
         def codegen(context, builder, sig, args):
             fndesc = funcdesc.ExternalFunctionDescriptor(
-                self.symbol, sig.return_type, sig.args
+                self.name, sig.return_type, sig.args
             )
             func = context.declare_external_function(builder.module, fndesc)
             return builder.call(func, args)
 
-        extending.lower_builtin(self.symbol, *self.signature.args)(codegen)
+        extending.lower_builtin(self.name, *self.signature.args)(codegen)
 
     def __call__(self, *args, **kwargs):
         """
         This is only defined to pretend to be a callable from CPython.
         """
-        msg = f"{self.symbol} is not usable in pure-python"
+        msg = f"{self.name} is not usable in pure-python"
         raise NotImplementedError(msg)
 
     @property
@@ -52,25 +77,5 @@ class _External:
     def signature(self):
         return self._signature
 
-    @property
-    def name(self):
-        return self.symbol
 
-
-def external(signature, name=None):
-
-    # Make inner function for the actual work
-    t = Type.fromobject(signature)
-    if not t.is_function:
-        raise ValueError("signature must represent a function type")
-
-    if name is None:
-        name = t.name
-    if not name:
-        raise ValueError(
-            f"external function name not specified for signature {signature}"
-        )
-
-    llc = _External(name, t.tonumba())
-    llc.register()
-    return llc
+external = External.fromobject
