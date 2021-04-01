1042b23047f73d958cfe4aa5b1c92aa149fc20d6
Pearu Peterson <pearu.peterson@gmail.com>
2021-04-01 13:09:22 +0300

    rbc supports python>=3.7, numba>=0.52

--- rbc/externals/libdevice.py
+++ rbc/externals/libdevice.py
@@ -1,6 +1,7 @@
 from rbc.utils import get_version
 from . import register_external
 from numba.core import imputils, typing  # noqa: E402
+from numba.cuda import libdevicefuncs  # noqa: E402
 
 # Typing
 typing_registry = typing.templates.Registry()
@@ -8,11 +9,7 @@ typing_registry = typing.templates.Registry()
 # Lowering
 lowering_registry = imputils.Registry()
 
-
-if get_version("numba") >= (0, 52):
-    from numba.cuda import libdevicefuncs  # noqa: E402
-
-    for fname, (retty, args) in libdevicefuncs.functions.items():
-        argtys = tuple(map(lambda x: f"{x.ty}*" if x.is_ptr else f"{x.ty}", args))
-        register_external(
-            fname, retty, argtys, __name__, globals(), typing_registry, lowering_registry)
+for fname, (retty, args) in libdevicefuncs.functions.items():
+    argtys = tuple(map(lambda x: f"{x.ty}*" if x.is_ptr else f"{x.ty}", args))
+    register_external(
+        fname, retty, argtys, __name__, globals(), typing_registry, lowering_registry)
