3c1a315703d89b1c9161cbbad3ea393898e4ff0b
Pearu Peterson <pearu.peterson@gmail.com>
2021-04-01 12:17:32 +0300

    Backport to numba 0.50

--- rbc/externals/libdevice.py
+++ rbc/externals/libdevice.py
@@ -1,9 +1,6 @@
 from rbc.utils import get_version
 from . import register_external
-
-assert get_version("numba") >= (0, 52)
 from numba.core import imputils, typing  # noqa: E402
-from numba.cuda import libdevicefuncs  # noqa: E402
 
 # Typing
 typing_registry = typing.templates.Registry()
@@ -12,8 +9,10 @@ typing_registry = typing.templates.Registry()
 lowering_registry = imputils.Registry()
 
 
-for fname, (retty, args) in libdevicefuncs.functions.items():
-    argtys = tuple(map(lambda x: f"{x.ty}*" if x.is_ptr else f"{x.ty}", args))
-    register_external(
-        fname, retty, argtys, __name__, globals(), typing_registry, lowering_registry
-    )
+if get_version("numba") >= (0, 52):
+    from numba.cuda import libdevicefuncs  # noqa: E402
+
+    for fname, (retty, args) in libdevicefuncs.functions.items():
+        argtys = tuple(map(lambda x: f"{x.ty}*" if x.is_ptr else f"{x.ty}", args))
+        register_external(
+            fname, retty, argtys, __name__, globals(), typing_registry, lowering_registry)
