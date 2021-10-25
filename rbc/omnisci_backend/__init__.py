9c34543bc7f06571ccb5b7866531e5c4c8cd17ba
Guilherme Leobas <gleobas@quansight.com>
2021-10-25 20:36:06 +0000

    fix for mathimpl failure

--- rbc/omnisci_backend/__init__.py
+++ rbc/omnisci_backend/__init__.py
@@ -6,6 +6,8 @@ from .omnisci_bytes import *  # noqa: F401, F403
 from .omnisci_metatype import *  # noqa: F401, F403
 from .omnisci_text_encoding import *  # noqa: F401, F403
 from .numpy_ufuncs import *  # noqa: F401, F403
+from .mathimpl import *  # noqa: F401, F403
+from .npyimpl import *  # noqa: F401, F403
 from .omnisci_pipeline import *  # noqa: F401, F403
 from .python_operators import *  # noqa: F401, F403
 from .omnisci_column_list import *  # noqa: F401, F403
