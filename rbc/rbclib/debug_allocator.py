3018748385a43d202e5b43c4739adb9afb2f2112
Antonio Cuni <anto.cuni@gmail.com>
2021-12-03 14:55:05 +0000

    write unit tests for DebugAllocator

--- rbc/rbclib/debug_allocator.py
+++ rbc/rbclib/debug_allocator.py
@@ -1,20 +1,37 @@
 from ._rbclib import lib, ffi
 
+class DebugAllocatorError(Exception):
+    pass
+
+class InvalidFreeError(DebugAllocatorError):
+    pass
+
 
 class DebugAllocator:
+    """
+    Provide debug versions of allocate_varlen_buffer and free_buffer to detect
+    memory leak.
+
+    The logic is written in pure Python, and it is exposed to the C world
+    through CFFI's @def_extern() mechanism.
+    """
 
     def __init__(self):
-        self.allocs = {}
-        self.n = 0
+        # alive_memory is a dictionary which contains all the addresses which
+        # have been allocated but not yet freed.  For each address we record
+        # an unique sequence number which acts as a timestamp, so that we can
+        # inspect them in allocation order.
+        self.seq = 0
+        self.alive_memory = {}  # {address: seq}
 
     def record_allocate(self, addr):
-        self.allocs[addr] = self.n
-        self.n += 1
+        self.seq += 1
+        self.alive_memory[addr] = self.seq
 
     def record_free(self, addr):
-        if addr not in self.allocs:
-            raise Exception('Trying to free() a dangling pointer?')
-        del self.allocs[addr]
+        if addr not in self.alive_memory:
+            raise InvalidFreeError('Trying to free() a dangling pointer?')
+        del self.alive_memory[addr]
 
 
 # global singleton
@@ -24,7 +41,7 @@ _ALLOCATOR = DebugAllocator()
 @ffi.def_extern()
 def rbclib_debug_allocate_varlen_buffer(element_count, element_size):
     addr = lib.rbclib_allocate_varlen_buffer(element_count, element_size)
-    _ALLOCATOR.record_allocate()
+    _ALLOCATOR.record_allocate(addr)
     return addr
 
 
--- rbc/tests/test_rbclib.py
+++ rbc/tests/test_rbclib.py
@@ -1,4 +1,6 @@
+import pytest
 from rbc import rbclib
+from rbc.rbclib.debug_allocator import DebugAllocator, InvalidFreeError
 from .test_numpy_rjit import rjit  # noqa: F401
 
 
@@ -12,3 +14,35 @@ def test_add_ints_rjit(rjit):  # noqa: F811
     def my_add(a, b):
         return rbclib.add_ints(a, b)
     assert my_add(20, 22) == 42
+
+
+class TestDebugAllocator:
+
+    def test_record_allocate(self):
+        allocator = DebugAllocator()
+        allocator.record_allocate(0x123)
+        allocator.record_allocate(0x456)
+        assert allocator.seq == 2
+        assert allocator.alive_memory == {0x123: 1, 0x456: 2}
+
+    def test_record_free(self):
+        allocator = DebugAllocator()
+        allocator.record_allocate(0x123)
+        allocator.record_allocate(0x456)
+        allocator.record_free(0x123)
+        allocator.record_allocate(0x789)
+        assert allocator.seq == 3
+        assert allocator.alive_memory == {0x456: 2, 0x789: 3}
+
+    def test_invalid_free(self):
+        allocator = DebugAllocator()
+        # 1. raise in case we try to free an unknown pointer
+        with pytest.raises(InvalidFreeError):
+            allocator.record_free(0x123)
+
+        # 2. raise in case of double free
+        allocator.record_allocate(0x456)
+        allocator.record_free(0x456)
+        assert allocator.alive_memory == {}
+        with pytest.raises(InvalidFreeError):
+            allocator.record_free(0x456)
