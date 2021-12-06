b2cc5066c1d452eb9fda01d9e4a23ffd2fcac124
Guilherme Leobas <guilhermeleobas@gmail.com>
2021-12-06 16:32:31 -0300

    flake8

--- rbc/omniscidb.py
+++ rbc/omniscidb.py
@@ -296,8 +296,8 @@ class RemoteOmnisci(RemoteJIT):
             version = self.thrift_call('get_version')
             self._version = parse_version(version)
             if self._version[:2] < (5, 6):
-                msg = (f'OmniSciDB server v.{version} is too old (expected v.5.6 or newer) and some '
-                       'features might not be available.')
+                msg = (f'OmniSciDB server v.{version} is too old (expected v.5.6 or newer) '
+                       'and some features might not be available.')
                 warnings.warn(msg, PendingDeprecationWarning)
         return self._version
 
