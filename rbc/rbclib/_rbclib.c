152f2519bc4e8212cc18e850d4d97b5964893926
Antonio Cuni <anto.cuni@gmail.com>
2021-12-07 15:15:27 +0000

    refactor and rationalize the declaration of rbclib functions
    Moreover, introdude RBC_DLLEXPORT, becasue windows require symbols to be
    exported explicitly

--- rbc/rbclib/_rbclib.c
+++ rbc/rbclib/_rbclib.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
-#include <stdint.h>
+#include "_rbclib.h"
+
 
 // trivial function, used to test that the basic machinery works
-int64_t _rbclib_add_ints(int64_t a, int64_t b) {
+RBC_DLLEXPORT int64_t _rbclib_add_ints(int64_t a, int64_t b) {
     return a + b;
 }
 
 // NOTE: allocate_varlen_buffer must have the same signature as the one
 // defined by omniscidb
-int8_t *rbclib_allocate_varlen_buffer(int64_t element_count, int64_t element_size) {
+RBC_DLLEXPORT int8_t *rbclib_allocate_varlen_buffer(int64_t element_count, int64_t element_size) {
     size_t size = element_count * element_size;
     // malloc(0) is allowed to return NULL. But here we want to ensure that we
     // return NULL only to signal an out of memory error, so we make sure to
@@ -18,7 +19,7 @@ int8_t *rbclib_allocate_varlen_buffer(int64_t element_count, int64_t element_siz
     return (int8_t *)malloc(size);
 }
 
-void rbclib_free_buffer(int8_t *addr) {
+RBC_DLLEXPORT void rbclib_free_buffer(int8_t *addr) {
     // XXX: currently there is a bug in the FreeOmnisciBuffer and the free()
     // is never called.  We should remove the abort() as soon as this code is
     // actually called
