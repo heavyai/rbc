15f4a34317d200bd97f48e91673b2064d8705b04
Antonio Cuni <anto.cuni@gmail.com>
2021-12-22 15:07:06 +0000

    Rename free_omnisci_buffer and FreeOmnisciBuffers into free_all_other_buffers and AutoFreeBuffers
    I think the original names are very confusing, and also add docstrings
    to explain what's going on

--- rbc/omnisci_backend/omnisci_buffer.py
+++ rbc/omnisci_backend/omnisci_buffer.py
@@ -194,8 +194,20 @@ def omnisci_buffer_constructor(context, builder, sig, args):
 
 
 @extending.intrinsic
-def free_omnisci_buffer(typingctx, ret):
-    sig = types.void(ret)
+def free_all_other_buffers(typingctx, value_to_keep_alive):
+    """
+    Black magic function which automatically frees all the buffers which were
+    allocated in the function apart the given one.
+
+    value_to_keep_alive can be of any type:
+      - if it's of a Buffer type, it will be kept alive and not freed
+      - if it's of any other type, all buffers will be freed unconditionally
+
+    The end user should never call this function explicitly: it is
+    automatically inserted by omnisci_pipeline.AutoFreeBuffers.
+    """
+
+    sig = types.void(value_to_keep_alive)
 
     def codegen(context, builder, signature, args):
         buffers = builder_buffers[builder]
@@ -211,16 +223,16 @@ def free_omnisci_buffer(typingctx, ret):
         free_buffer_fn = irutils.get_or_insert_function(
             builder.module, free_buffer_fnty, free_buffer_fn_name)
 
-        # We skip the ret pointer iff we're returning a Buffer
-        # otherwise, we free everything
-        if isinstance(ret, BufferPointer):
-            [arg] = args
-            ptr = builder.load(builder.gep(arg, [int32_t(0), int32_t(0)]))
-            ptr = builder.bitcast(ptr, int8_t.as_pointer())
+        if isinstance(value_to_keep_alive, BufferPointer):
+            # free all the buffers apart value_to_keep_alive
+            [keep_alive] = args
+            keep_alive_ptr = builder.load(builder.gep(keep_alive, [int32_t(0), int32_t(0)]))
+            keep_alive_ptr = builder.bitcast(keep_alive_ptr, int8_t.as_pointer())
             for ptr8 in buffers:
-                with builder.if_then(builder.icmp_signed('!=', ptr, ptr8)):
+                with builder.if_then(builder.icmp_signed('!=', keep_alive_ptr, ptr8)):
                     builder.call(free_buffer_fn, [ptr8])
         else:
+            # free all the buffers unconditionally
             for ptr8 in buffers:
                 builder.call(free_buffer_fn, [ptr8])
 
