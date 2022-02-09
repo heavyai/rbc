bf23f4aadfde56ebbeab0d083c9e33ae8a071eb1
Pamphile Roy <roy.pamphile@gmail.com>
2022-02-09 17:13:27 +0100

    MAINT: some noqa

--- rbc/omnisci_backend/__init__.py
+++ rbc/omnisci_backend/__init__.py
@@ -9,8 +9,8 @@ from .omnisci_pipeline import *  # noqa: F401, F403
 from .omnisci_column_list import *  # noqa: F401, F403
 from .omnisci_table_function_manager import *  # noqa: F401, F403
 
-import rbc.omnisci_backend.mathimpl as math
-import rbc.omnisci_backend.npyimpl as np
-import rbc.omnisci_backend.python_operators as operators
+import rbc.omnisci_backend.mathimpl as math  # noqa: F401
+import rbc.omnisci_backend.npyimpl as np  # noqa: F401
+import rbc.omnisci_backend.python_operators as operators  # noqa: F401
 
 __all__ = [s for s in dir() if not s.startswith('_')]
