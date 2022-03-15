3cd9c855d0cc5e1d23b9a8b5c9a85bc45a0af8fc
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-03-15 19:07:23 -0300

    redo array table of contents

--- rbc/stdlib/array_attributes.py
+++
@@ -1,7 +0,0 @@
-"""
-Array API specification for array attributes.
-
-https://data-apis.org/array-api/latest/API_specification/array_object.html#attributes
-"""
-
-__all__ = []
--- rbc/stdlib/array_methods.py
+++
@@ -1,7 +0,0 @@
-"""
-Array API specification for array methods.
-
-https://data-apis.org/array-api/latest/API_specification/data_type_functions.html
-"""
-
-__all__ = []
