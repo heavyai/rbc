e48b5a3cc6f787cdbde71a658e3840c75181202b
Pamphile Roy <roy.pamphile@gmail.com>
2022-03-17 16:55:24 +0100

    Relative imports and improve warning message
    Co-authored-by: Pearu Peterson <pearu.peterson@gmail.com>

--- rbc/omniscidb.py
+++ rbc/omniscidb.py
@@ -2,5 +2,5 @@ import warnings  # noqa: F401
 from .heavydb import *  # noqa: F401, F403
 
 
-msg = "`RemoteOmnisci` is deprecated, use `RemoteHeavyDB` instead."
+msg = "`import rbc.omniscidb` is deprecated, use `import rbc.heavyai` instead."
 warnings.warn(msg, PendingDeprecationWarning)
