c576db2714c950b31bf799590d91bd42df768b57
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-11-16 16:34:22 -0300

    update thrift file to add recent type definitions (#518)

--- rbc/extension_functions.thrift
+++ rbc/extension_functions.thrift
@@ -79,6 +79,18 @@ enum TExtArgumentType {
   GeoMultiPoint,
   DayTimeInterval,
   YearMonthTimeInterval,
+  ColumnGeoPoint,
+  ColumnGeoLineString,
+  ColumnGeoPolygon,
+  ColumnGeoMultiPoint,
+  ColumnGeoMultiLineString,
+  ColumnGeoMultiPolygon,
+  ColumnListGeoPoint,
+  ColumnListGeoLineString,
+  ColumnListGeoPolygon,
+  ColumnListGeoMultiPoint,
+  ColumnListGeoMultiLineString,
+  ColumnListGeoMultiPolygon,
 }
 
 /* See QueryEngine/TableFunctions/TableFunctionsFactory.h for required
